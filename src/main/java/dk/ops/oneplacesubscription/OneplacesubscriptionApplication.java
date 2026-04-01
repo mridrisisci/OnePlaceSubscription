bde67736b83d51eed8a491a1485a509e18f1001f
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2026-04-01 12:38:07 +0100

    1st

---
+++ src/main/java/dk/ops/oneplacesubscription/OneplacesubscriptionApplication.java
@@ -0,0 +1,15 @@
+package dk.ops.oneplacesubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneplacesubscriptionApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OneplacesubscriptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/ops/oneplacesubscription/OneplacesubscriptionApplicationTests.java
@@ -0,0 +1,15 @@
+package dk.ops.oneplacesubscription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneplacesubscriptionApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
